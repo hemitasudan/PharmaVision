999c306a04247cbfd22994ecc07028233967f419
hemitasudan <145245957+hemitasudan@users.noreply.github.com>
2025-02-26 23:40:57 +0530

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import shutil
+
+# load css file
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+local_css("styles.css")
+
+st.header("Welcome to PharmaVision")
+
+st.subheader("Decode Doctor's Handwriting Instantly!")
+st.text("Are you tired of struggling with illegible prescriptions? Our AI-powered solution converts handwritten doctor's prescriptions into clear, structured text in seconds!")
+st.text("Just head over to the Read Prescription page and upload an image of the prescription to get instant ressults.")
+
+st.subheader("Wanna know more about your medicine?")
+st.text("Head to the Drug Assistant page and ask away! Know all about the side effects and other details of your prescription.")
---
+++ pages/Drug_Assistant.py
@@ -0,0 +1,53 @@
+import streamlit as st
+from transformers import pipeline
+import os
+import openai
+from openai import OpenAI
+from keys import OPENAI_API_KEY
+import shutil
+
+
+# Initialize the model
+os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
+client = openai.OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+# load css file
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+local_css("styles.css")
+
+# Function to fetch drug info
+def get_drug_info(question):
+
+    prompt = f'''If the following question is medicine related, answer it, otherwise say that you can only answer medicinal questions because you're a pharmaceutical assistant.
+    The question is: {question}'''
+    print("")
+    # Generate response
+    response = client.chat.completions.create(
+    model="gpt-4o",
+    messages=[
+        {
+            "role": "user",
+            "content": prompt
+        }])
+    return response.choices[0].message.content
+
+# Streamlit UI
+st.subheader("Familiarize yourself with your medication")
+
+# Input for questions
+medications_input = st.text_area("Ask anything about your medicine!")
+
+if st.button("Submit"):
+    if medications_input:
+
+        # Generate response from the model
+        with st.spinner('Generating response...'):
+            result = get_drug_info(medications_input)
+        
+        # Display the result
+        st.subheader("Results")
+        st.write(result)
+    else:
+        st.error("Please type your query.")
